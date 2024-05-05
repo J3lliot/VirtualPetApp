33e7d66a6ac325c1f1ca159bb2bf443c4daae6c5
J3lliot <st10441495@vcconnect.edu.za>
2024-05-05 17:51:45 +0200

    Check Fixed Code

--- app/src/main/java/com/example/virtualpet/MainActivity2.kt
+++ app/src/main/java/com/example/virtualpet/MainActivity2.kt
@@ -9,6 +9,12 @@ import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
 
+    private var health = 100
+    private var hunger = 0
+    private var cleanliness = 100
+    private val feedTxt = findViewById<TextView>(R.id.hunger)
+    private val healthTxt = findViewById<TextView>(R.id.health)
+    private  val cleanTxt = findViewById<TextView>(R.id.cleanliness)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
@@ -31,34 +37,37 @@ class MainActivity2 : AppCompatActivity() {
             cleanPet()
             imageView.setImageResource(R.drawable.img_4)
         }
+        updateStatusText()
     }
-    private var health = 100
-    private var hunger = 0
-    private  var cleanliness = 100
 
-    fun updateStatusText() {}
 
-    fun feedPet() {
-        hunger = 20
-        if (hunger > 0) hunger = 0
+    private fun updateStatusText() {
+        healthTxt.text = "Health - $health"
+        feedTxt.text = "Hunger - $hunger"
+        cleanTxt.text = "Cleanliness - $cleanliness"
+    }
+
+    private fun feedPet() {
+        hunger -= 20
+        if (hunger < 0) hunger = 0
         health += 10
         if (health > 100) health = 100
         updateStatusText()
     }
 
 
-        private fun cleanPet() {
-            cleanliness = 100
-            health += 10
-            if (health > 100) health = 100
-            updateStatusText()
-        }
+    private fun cleanPet() {
+        cleanliness = 100
+        health += 10
+        if (health > 100) health = 100
+        updateStatusText()
+    }
 
-        private fun playWithPet() {
-            health -= 15
-            if (health > 0) health = 0
-            hunger += 15
-            if (hunger > 100) hunger = 100
-            updateStatusText()
-        }
-        }
+    private fun playWithPet() {
+        health -= 15
+        if (health < 0) health = 0
+        hunger += 15
+        if (hunger > 100) hunger = 100
+        updateStatusText()
+    }
+}
